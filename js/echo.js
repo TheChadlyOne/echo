0d8af9f7ef3043ed8fa63a6873c28c536a9dc28c
Chad H <chadroberthall@gmail.com>
2015-01-24 18:17:52 -0700

    First Commit

--- js/echo.js
+++ js/echo.js
@@ -1,6 +1,12 @@
 function echo()
 {
-  // ...
+//chadly here
+    console.log(arguments); 
+    var htmlOutput = "";
+    for (var i=0; i < arguments.length; i++) {
+        htmlOutput += arguments[i] + '<br>';
+    }
+    document.write('<p>'+htmlOutput+'</p>' );
 }
 
 echo();
--- php/echo.php
+++ php/echo.php
@@ -1,10 +1,27 @@
-<?php
 
-function echo()
+<!-- Chadly Here -->
+
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+<?php
+function eecho()
 {
-  // ...
+    $numargs = func_num_args();
+
+    $arg_list = func_get_args();
+    echo "<p>";
+    for ($i = 0; $i < $numargs; $i++) {
+        echo $arg_list[$i] . "<br />";
+    }
+    echo "</p>";
 }
 
-echo();
-echo('bla');
-echo('foo', 'bar', 'baz');
+eecho();
+eecho('bla');
+eecho('foo', 'bar', 'baz');
+?>
+ </body>
+</html>
